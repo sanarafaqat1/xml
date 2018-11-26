b020397bd448aec3412f4e6dc34c16c85623c6d7
Sana Rafaqat <sanarafaqat041@gmail.com>
2018-11-26 12:03:46 +0000

    V.0.0.2 Used Xpath and Search

--- xml_intro/xml_parsing.rb
+++ xml_intro/xml_parsing.rb
@@ -5,7 +5,26 @@ class GuiseppesMenu
   def initialize
     @menu = Nokogiri::XML(File.open('xml_menu.xml'))
   end
+  def get_menu_names
+    @menu.search('name')
+  end
+  def get_menu_food_item
+    @menu.search('food')
+  end
+  #Absolute Path for calories
+  def get_xpath_calories
+    @menu.xpath('/breakfast_menu/food/calories')
+  end
+  def get_xpath_names
+    @menu.xpath('//name')
+  end
 end
 
 guiseppes = GuiseppesMenu.new
-p guiseppes.menu
+puts guiseppes.get_menu_names[0]
+puts guiseppes.get_menu_names.length
+puts guiseppes.get_menu_names.last.text
+puts guiseppes.get_xpath_calories[0].text
+puts guiseppes.get_xpath_names[1].text
+
+# puts guiseppes.get_menu_food_item
