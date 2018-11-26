b61dabc2b43baafaab7ca954a696e8935792d151
Sana Rafaqat <sanarafaqat041@gmail.com>
2018-11-26 11:39:52 +0000

    Created a Initialize using Nokogiri gem

--- xml_intro/xml_parsing.rb
+++ xml_intro/xml_parsing.rb
@@ -1,9 +1,11 @@
-require 'nokogiri'
+require 'Nokogiri'
 
 class GuiseppesMenu
   attr_accessor :menu
   def initialize
-    
+    @menu = Nokogiri::XML(File.open('xml_menu.xml'))
   end
-
 end
+
+guiseppes = GuiseppesMenu.new
+p guiseppes.menu
